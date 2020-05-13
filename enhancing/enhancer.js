6a668e4aa0128f2bf1181c8aaef40f5f35e1ad33
Lee Damien <notesong.git@outlook.com>
2020-05-12 22:50:16 -0400

    Tests and functions

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,92 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  validateNumbers(item);
+
+  // add 1 to enhancement if less than 20
+  if (item.enhancement < 20) {
+    return { ...item, enhancement: item.enhancement + 1 };
+    // enhancement is unchanged if 20
+  } else {
+    return { ...item };
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  validateNumbers(item);
+
+  // check item numbers for validity
+  if (item.durability < 0) {
+    item = { ...item, durability: 0 };
+  } else if (item.durability > 100) {
+    item = { ...item, durability: 100 };
+  }
+  if (item.enhancement > 20) {
+    item = { ...item, enhancement: 20 };
+  } else if (item.enhancement < 0) {
+    item = { ...item, enhancement: 0 };
+  }
+
+  if (item.enhancement < 15) {
+    // if durability is less than 5, set durability to 0
+    if (item.durability < 5) {
+      return { ...item, durability: 0 };
+      // if durability is 5 or greater, take 5 off of durability
+    } else {
+      return { ...item, durability: item.durability - 5 };
+    }
+  } else {
+    // if enhancement is greater than 16, deduct a point from enhancement
+    if (item.enhancement > 16) {
+      // if durability is less than 10, set durability to 0
+      if (item.durability < 10) {
+        return {
+          ...item,
+          durability: 0,
+          enhancement: item.enhancement - 1,
+        };
+        // if durability is 10 or greater, take 10 off of durability
+      } else {
+        return {
+          ...item,
+          durability: item.durability - 10,
+          enhancement: item.enhancement - 1,
+        };
+      }
+      // if enhancement is 16 or less, don't deduct a point from enhancement
+    } else {
+      // if durability is less than 10, set durability to 0
+      if (item.durability < 10) {
+        return {
+          ...item,
+          durability: 10,
+        };
+        // if durability is 10 or greater, take 10 off of durability
+      } else {
+        return {
+          ...item,
+          durability: item.durability - 10,
+        };
+      }
+    }
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  validateNumbers(item);
+  // set durability to 100 no matter what durability currently is
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
   return { ...item };
+}
+
+function validateNumbers(item) {
+  if (
+    !Number.isInteger(item.durability) ||
+    !Number.isInteger(item.enhancement)
+  ) {
+    throw new Error("Expected a number");
+  }
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,143 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const enhancer = require("./enhancer.js");
+
+describe("enhancer item tests", () => {
+  it("successful", () => {
+    checkNumberValidity();
+
+    // add 1 to enhancement if less than 20
+    let succeeded = enhancer.succeed({
+      name: "ice",
+      durability: 100,
+      enhancement: 10,
+    });
+    expect(succeeded.enhancement).toBe(11);
+
+    // enhancement is unchanged if 20
+    succeeded = enhancer.succeed({
+      name: "ice",
+      durability: 100,
+      enhancement: 20,
+    });
+    expect(succeeded.enhancement).toBe(20);
+  });
+
+  it("fails", () => {
+    checkNumberValidity();
+
+    //subtract 5 durability and no loss to enhancement
+    let failed = enhancer.fail({
+      name: "ice",
+      durability: 80,
+      enhancement: 10,
+    });
+    expect(failed.durability).toBe(75);
+    expect(failed.enhancement).toBe(10);
+
+    // subtract 10 durability and -1 to enhancement
+    // because less than 10 durability, durability becomes 0 automatically
+    failed = enhancer.fail({
+      name: "ice",
+      durability: 9,
+      enhancement: 20,
+    });
+    expect(failed.durability).toBe(0);
+    expect(failed.enhancement).toBe(19);
+
+    // subtract 10 durability and -1 to enhancement
+    failed = enhancer.fail({
+      name: "ice",
+      durability: 80,
+      enhancement: 20,
+    });
+    expect(failed.durability).toBe(70);
+    expect(failed.enhancement).toBe(19);
+
+    // subtract 5 durability and no loss to enhancement
+    // because less than 5 durability, durability becomes 0 automatically
+    failed = enhancer.fail({
+      name: "ice",
+      durability: 4,
+      enhancement: 10,
+    });
+    expect(failed.durability).toBe(0);
+    expect(failed.enhancement).toBe(10);
+
+    // subtract 5 durability and no loss to enhancement
+    // because less than 5 durability, durability becomes 0 automatically
+    failed = enhancer.fail({
+      name: "ice",
+      durability: 60,
+      enhancement: 10,
+    });
+    expect(failed.durability).toBe(55);
+    expect(failed.enhancement).toBe(10);
+
+    ////////////////////////////////
+    // check for invalid numbers
+
+    // invalid durability amounts
+    failed = enhancer.fail({
+      name: "ice",
+      durability: -1,
+      enhancement: 10,
+    });
+    expect(failed.durability).toBe(0);
+    expect(failed.enhancement).toBe(10);
+
+    failed = enhancer.fail({
+      name: "ice",
+      durability: 101,
+      enhancement: 10,
+    });
+    expect(failed.durability).toBe(95);
+    expect(failed.enhancement).toBe(10);
+
+    // invalid enhancement amounts
+    failed = enhancer.fail({
+      name: "ice",
+      durability: 10,
+      enhancement: -1,
+    });
+    expect(failed.durability).toBe(5);
+    expect(failed.enhancement).toBe(0);
+  });
+
+  it("repairs", () => {
+    checkNumberValidity();
+
+    // set durability to 100 no matter what durability currently is
+    let repaired = enhancer.repair({
+      name: "ice",
+      durability: 80,
+      enhancement: 15,
+    });
+    expect(repaired.durability).toBe(100);
+
+    repaired = enhancer.repair({
+      name: "ice",
+      durability: 500,
+      enhancement: 15,
+    });
+    expect(repaired.durability).toBe(100);
+  });
+
+  it("gets", () => {
+    //expect(enhancer.get()).toBe(true);
+  });
+});
+
+function checkNumberValidity() {
+  let testObject = {
+    name: "ice",
+    durability: "60",
+    enhancement: 21,
+  };
+  expect(() => enhancer.fail(testObject)).toThrow();
+
+  testObject = {
+    name: "ice",
+    durability: 60,
+    enhancement: "21",
+  };
+  expect(() => enhancer.fail(testObject)).toThrow();
+}
