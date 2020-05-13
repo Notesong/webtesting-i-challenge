d539b1f930c7b4eea1daad7eab07b1b9f80f810b
Lee Damien <notesong.git@outlook.com>
2020-05-12 23:03:21 -0400

    Optimize code

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,31 +6,19 @@ module.exports = {
 };
 
 function succeed(item) {
-  validateNumbers(item);
+  item = validateNumbers(item);
 
   // add 1 to enhancement if less than 20
   if (item.enhancement < 20) {
     return { ...item, enhancement: item.enhancement + 1 };
     // enhancement is unchanged if 20
   } else {
-    return { ...item };
+    return { ...item, enhancement: 20 };
   }
 }
 
 function fail(item) {
-  validateNumbers(item);
-
-  // check item numbers for validity
-  if (item.durability < 0) {
-    item = { ...item, durability: 0 };
-  } else if (item.durability > 100) {
-    item = { ...item, durability: 100 };
-  }
-  if (item.enhancement > 20) {
-    item = { ...item, enhancement: 20 };
-  } else if (item.enhancement < 0) {
-    item = { ...item, enhancement: 0 };
-  }
+  item = validateNumbers(item);
 
   if (item.enhancement < 15) {
     // if durability is less than 5, set durability to 0
@@ -78,7 +66,7 @@ function fail(item) {
 }
 
 function repair(item) {
-  validateNumbers(item);
+  item = validateNumbers(item);
   // set durability to 100 no matter what durability currently is
   return { ...item, durability: 100 };
 }
@@ -88,10 +76,29 @@ function get(item) {
 }
 
 function validateNumbers(item) {
+  // validate numbers are a number and not a string
   if (
     !Number.isInteger(item.durability) ||
     !Number.isInteger(item.enhancement)
   ) {
     throw new Error("Expected a number");
   }
+
+  // check numbers are in accepted range
+  // if they're not, put them into the accepted range
+
+  // durability is between 0 and 100
+  if (item.durability < 0) {
+    item = { ...item, durability: 0 };
+  } else if (item.durability > 100) {
+    item = { ...item, durability: 100 };
+  }
+  // enhancement is between 0 and 20
+  if (item.enhancement > 20) {
+    item = { ...item, enhancement: 20 };
+  } else if (item.enhancement < 0) {
+    item = { ...item, enhancement: 0 };
+  }
+
+  return item;
 }
